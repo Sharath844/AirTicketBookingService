856b3532b470d5e1a6ff68ce28744d2d64aa08d3
Lingabathula Sharath <sharathlingabathula@gmail.com>
2023-03-14 23:17:03 +0530

    Added rabbit mq config

--- src/controller/booking-controller.js
+++ src/controller/booking-controller.js
@@ -3,14 +3,36 @@ const { StatusCodes } = require('http-status-codes');
 
 const { BookingService } = require('../services/index');
 
+const {createChannel, publishMessage} = require('../utils/messageQueue');
+const {REMINDER_BINDING_KEY} = require('../config/serverConfig');
 
-const { REMINDER_BINDING_KEY } = require('../config/serverConfig');
 
 const bookingService = new BookingService();
 
 
+class BookingController {
+    constructor(){
 
-    const create = async (req, res) => {
+    }
+
+    async sendMessageToQueue(req, res){
+        const channel = await createChannel();
+        const payload = {
+            data: {
+               subject: 'This is a noti from queue',
+               content: 'Some queue will subcribe this',
+               recepientEmail: 'cs191297@gmail.com',
+               notificationTime: '2023-03-14T17:19:16.628Z'
+            },
+            service: 'CREATE_TICKET'
+        };
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        return res.status(200).json({
+            message: 'Succesfully published the event'
+        });
+    }
+
+    async create(req, res){
         try {
             const response = await bookingService.createBooking(req.body);
             console.log("FROM BOOKING CONTROLLER", response);
@@ -30,7 +52,5 @@ const bookingService = new BookingService();
         }
     }
 
-
-module.exports = {
-    create
 }
+module.exports = BookingController;
